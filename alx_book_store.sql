a799e7735c1d6632a270d17c5082ace2fe09243a
Marwan_Issa <missa4312@gmail.com>
2024-10-19 22:40:24 +0300

    edit in the database tables

--- alx_book_store.sql
+++ alx_book_store.sql
@@ -23,7 +23,7 @@ address TEXT);
 
 CREATE TABLE Orders(
 order_id INT PRIMARY KEY,
-FOREIGN KEY (customer_id) REFERENCES Customers (customer_id),
+FOREIGN KEY (customer_id) REFERENCES Customers(customer_id),
 order_date DATE);
 
 CREATE TABLE Order_Details (
