0e2c24bddcd6ee3ad7f6e165d72e65a75b2a58f2
Marwan_Issa <missa4312@gmail.com>
2024-10-19 22:41:59 +0300

    edit in the database tables

--- alx_book_store.sql
+++ alx_book_store.sql
@@ -28,6 +28,6 @@ order_date DATE);
 
 CREATE TABLE Order_Details (
 orderdetailid INT PRIMARY KEY,
-FOREIGN KEY (order_id) REFERENCES Orders (order_id),
-FOREIGN KEY (book_id) REFERENCES Books (book_id),
+FOREIGN KEY (order_id) REFERENCES Orders(order_id),
+FOREIGN KEY (book_id) REFERENCES Books(book_id),
 quantity DOUBLE);
